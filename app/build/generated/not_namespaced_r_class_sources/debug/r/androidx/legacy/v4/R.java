36fe2e7ee7105894cf8c13d4bb40beac1118d75c
ABAdkins <a.brown46464@gmail.com>
2020-03-03 16:03:53 -0600

    changed icon and name

--- app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/legacy/v4/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/legacy/v4/R.java
@@ -37,13 +37,13 @@ public final class R {
     public static final class color {
         private color() {}
 
-        public static final int notification_action_color_filter = 0x7f040051;
-        public static final int notification_icon_bg_color = 0x7f040052;
-        public static final int notification_material_background_media_default_color = 0x7f040053;
-        public static final int primary_text_default_material_dark = 0x7f040069;
-        public static final int ripple_material_light = 0x7f0401c5;
-        public static final int secondary_text_default_material_dark = 0x7f0401c6;
-        public static final int secondary_text_default_material_light = 0x7f0401c7;
+        public static final int notification_action_color_filter = 0x7f040052;
+        public static final int notification_icon_bg_color = 0x7f040053;
+        public static final int notification_material_background_media_default_color = 0x7f040054;
+        public static final int primary_text_default_material_dark = 0x7f04006a;
+        public static final int ripple_material_light = 0x7f0401c6;
+        public static final int secondary_text_default_material_dark = 0x7f0401c7;
+        public static final int secondary_text_default_material_light = 0x7f0401c8;
     }
     public static final class dimen {
         private dimen() {}
--- app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/vectordrawable/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/r/androidx/vectordrawable/R.java
@@ -37,10 +37,10 @@ public final class R {
     public static final class color {
         private color() {}
 
-        public static final int notification_action_color_filter = 0x7f040051;
-        public static final int notification_icon_bg_color = 0x7f040052;
-        public static final int ripple_material_light = 0x7f0401c5;
-        public static final int secondary_text_default_material_light = 0x7f0401c7;
+        public static final int notification_action_color_filter = 0x7f040052;
+        public static final int notification_icon_bg_color = 0x7f040053;
+        public static final int ripple_material_light = 0x7f0401c6;
+        public static final int secondary_text_default_material_light = 0x7f0401c8;
     }
     public static final class dimen {
         private dimen() {}
